2a6004fba0cab981d81be0bcf20a4a9e285f3b80
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-02-04 18:15:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+document.getElementById("votingForm").addEventListener('submit',(e)=>{
+	e.preventDefault();
+	const age=document.getElementById("age").value;
+	const name=document.getElementById("name").value;
+	if(age=="" && name==""){
+		alert("Please enter valid details");
+		return;
+	}else{
+		let promise=new Promise((resolve,reject)=>{
+			if(parseInt(age)>18){
+				setTimeout(()=>{
+					resolve(`Welcome,${name}. You can vote.`);
+				},4000)
+			}else{
+				setTimeout(()=>{
+					reject("Oh sorry Doe. You aren't old enough.");
+				},4000)
+			}
+		});
+		promise.then((data)=>{
+			alert(data);
+		}).catch((err)=>{
+			alert(err);
+		})
+	}
+})
